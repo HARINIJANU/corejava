0dd81e8639117f327a7fead8be9de3e4048bbe6f
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:31:08 -0700

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,17 @@
+package com.edu;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      float a,b,c;
+      a=12.3f;
+      b=16.2f;
+      
+      c=a/b;
+       
+      System.out.println("c="+c);
+      
+	}
+
+}

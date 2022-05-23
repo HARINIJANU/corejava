13a225f91fecf2d60e51845a1c1bc467a9273149
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:30:06 -0700

    Add files via upload

---
+++ AllWorkout.java
@@ -0,0 +1,21 @@
+package com.edu;
+
+public class AllWorkout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      float a,b,c,d,e,f;
+      a=18.2f;
+      b=12.34f;
+      c=a+b;
+      d=a-b;
+      e=a*b;
+      f=a/b;
+      System.out.println("addition of two numbers of "+a+" and "+b+" is "+c);
+      System.out.println("subtraction of two numbers of "+a+" and "+b+" is "+d);
+      System.out.println("multiplication of two numbers of "+a+" and "+b+" is "+e);
+      System.out.println("divison of two numbers of "+a+" and "+b+" is"+f);
+
+	}
+
+}

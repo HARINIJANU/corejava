75e8cb2b27b2e61ec4facaa8a76d753d23d32619
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:21:13 -0700

    Add files via upload

---
+++ AreaOfSquare.java
@@ -0,0 +1,14 @@
+package hari1.edu;
+
+public class AreaOfSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      float a,b;
+      a=23.96f;
+      b=a*a;
+      
+      System.out.println("area of square of "+a+" is "+b);
+	}
+
+}

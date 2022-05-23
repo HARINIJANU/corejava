25d332be893d924970d0d7d94e04fa29511a4428
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:20:29 -0700

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,18 @@
+package hari1.edu;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float f,a;
+     int r;
+     r=23;
+     f=3.14159f;
+     a=f*r*r;
+     
+     System.out.println("area of circle is "+a);
+     
+     
+	}
+
+}

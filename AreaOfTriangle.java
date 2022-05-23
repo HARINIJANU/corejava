1b9d77898c3adc1ffc8781c77260987132f59bae
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:21:32 -0700

    Add files via upload

---
+++ AreaOfTriangle.java
@@ -0,0 +1,18 @@
+package hari1.edu;
+
+public class AreaOfTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+    float f,a;
+    int b,h;
+    b=14;
+    h=23;
+    f=0.5f;
+    a=f*b*h;
+    System.out.println("area of triangle "+b+" and "+h+" is "+a);
+    
+	}
+
+}

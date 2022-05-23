88333739f5e373e875f9d8535772dce9d62cb4c9
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:32:37 -0700

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,16 @@
+package com.edu;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a,b,c;
+      a=10;
+      b=16;
+      c=a-b;
+      
+      System.out.println("c= "+c );
+
+	}
+
+}

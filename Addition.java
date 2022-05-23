eb56d2ec81b77916e7df31bf8461a0fd2533bcf3
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:29:38 -0700

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a,b,c;
+        a=10;
+        b=15;
+        c=a+b;
+        
+        System.out.println("c=" +c);
+	}
+
+}

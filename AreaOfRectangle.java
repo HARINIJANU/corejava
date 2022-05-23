95bdc631106730cd80994edf98028689066b6cc4
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:20:51 -0700

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,15 @@
+package hari1.edu;
+
+public class AreaOfRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     float l,b,a;
+     l=12.2f;
+     b=15.4f;
+     a=l*b;
+     
+     System.out.println("Area of rectangle "+l+" and "+b+" is "+a);
+	}
+
+}

12059ab37688c5c1d76707ec2220e7b54acf330e
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:28:22 -0700

    Add files via upload

---
+++ PrintNames.java
@@ -0,0 +1,14 @@
+package hari5.edu;
+
+public class PrintNames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		i=1;
+		while(i<=100)
+			System.out.println(i++);
+
+	}
+
+}

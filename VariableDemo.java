68c27922f3c1bcb3e74e4f4713d37e9c0a79c831
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:32:57 -0700

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,20 @@
+package com.edu;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ival=10;
+		char gen='F';
+		float fval=12.36f;
+		String name="Harini";
+		
+		System.out.println("int value is "+ival);
+		System.out.println("char value is "+gen);
+		System.out.println("float value is "+fval);
+		System.out.println("Sting value is "+name);
+	   
+
+	}
+
+}

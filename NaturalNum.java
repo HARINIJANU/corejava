59cdc84d735e4fc66cc593fe8f495547103c2b0d
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-22 20:27:41 -0700

    Add files via upload

---
+++ NaturalNum.java
@@ -0,0 +1,18 @@
+package hari5.edu;
+
+public class NaturalNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int i,sum;
+     i=1;
+     sum=0;
+     while(i<=4)
+     {
+    	 sum=sum+i;
+         i++;
+	}
+     System.out.println("sum="+sum);
+	}
+
+}

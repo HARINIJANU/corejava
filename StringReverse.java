4aa01b71f4fec1d1f1c7acc93d7adbb2afa71545
HARINIJANU <105905557+HARINIJANU@users.noreply.github.com>
2022-05-29 05:15:49 -0700

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,21 @@
+package com.edu;
+
+public class StringReverse {
+
+	public static void main(String[] args)
+{
+		// TODO Auto-generated method stub
+  String s="edubridge";
+  System.out.println("no of char="+s.length());
+  {
+  
+	
+	for(int j=s.length()-1;j>=0;j--)
+	{
+		System.out.println(s.charAt(j));
+	}
+   
+
+}
+}
+	}
